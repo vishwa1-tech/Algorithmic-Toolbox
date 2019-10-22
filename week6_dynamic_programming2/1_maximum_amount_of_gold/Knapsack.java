485dc58230edc0ea4706677664f9baf56f27a030
Nikhil <50777800+INDALPHA@users.noreply.github.com>
2019-10-22 10:41:05 +0530

    Update Knapsack.java

--- week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
@@ -3,13 +3,31 @@ import java.util.*;
 public class Knapsack {
     static int optimalWeight(int W, int[] w) {
         //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+        Arrays.sort(w);
+        int T[][]=new int[w.length][W+1];
+        for(int i=0;i<w.length;i++){
+            for(int j=0;j<W+1;j++){
+                if(j==0){
+                    T[i][j]=0;
+                }
+                else if(i==0) {
+                    if (w[0] <= j) {
+                        T[i][j] = w[0];
+                    } else
+                        T[i][j] = 0;
+                }
+                else{
+                    if(w[i]>j){
+                        T[i][j]=T[i-1][j];
+                    }
+                    else{
+                        T[i][j]=Math.max(w[i]+T[i-1][j-w[i]],T[i-1][j]);
+                    }
+                }
+
+            }
         }
-        return result;
+        return T[w.length-1][W];
     }
 
     public static void main(String[] args) {
